8c5c640cfc38d6c6db010f0ae45709ceb22bb85c
hocgin <hocgin@gmail.com>
2020-08-17 21:43:32 +0800

    SSO Sample

---
+++ spring-boot-samples/sso-client-spring-boot-sample/src/main/java/in/hocg/sso/client/sample/SsoClientApplication.java
@@ -0,0 +1,19 @@
+package in.hocg.sso.client.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hocgin on 2020/8/15.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@SpringBootApplication
+public class SsoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoClientApplication.class, args);
+    }
+
+}
---
+++ spring-boot-samples/sso-server-spring-boot-sample/src/main/java/in/hocg/sso/server/sample/SsoServerApplication.java
@@ -0,0 +1,19 @@
+package in.hocg.sso.server.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hocgin on 2020/8/15.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@SpringBootApplication
+public class SsoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoServerApplication.class, args);
+    }
+
+}
