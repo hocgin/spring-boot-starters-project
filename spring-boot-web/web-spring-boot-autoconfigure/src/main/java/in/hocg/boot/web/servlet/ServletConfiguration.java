4741ed09183b6ed6d7147e959e60b811e6f0a694
hocgin <hocgin@gmail.com>
2020-08-18 21:53:16 +0800

    Code

--- spring-boot-web/web-spring-boot-autoconfigure/src/main/java/in/hocg/boot/web/servlet/ServletConfiguration.java
+++ spring-boot-web/web-spring-boot-autoconfigure/src/main/java/in/hocg/boot/web/servlet/ServletConfiguration.java
@@ -1,8 +1,10 @@
 package in.hocg.boot.web.servlet;
 
+import in.hocg.boot.web.SpringContext;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -19,4 +21,8 @@ import javax.servlet.Servlet;
 @ConditionalOnClass({Servlet.class, Tomcat.class})
 @Import(ServletExceptionAdvice.class)
 public class ServletConfiguration {
+    @Bean
+    public SpringContext springContext() {
+        return new SpringServletContext();
+    }
 }
---
+++ spring-boot-web/web-spring-boot-autoconfigure/src/main/java/in/hocg/boot/web/webflux/SpringWebFluxContext.java
@@ -0,0 +1,14 @@
+package in.hocg.boot.web.webflux;
+
+
+import in.hocg.boot.web.SpringContext;
+
+/**
+ * Created by hocgin on 2020/8/18
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+public class SpringWebFluxContext extends SpringContext {
+
+}
