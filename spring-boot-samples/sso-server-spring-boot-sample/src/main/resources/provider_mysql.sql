5d314aed9764e37fddebc2bd84923a1f9b852cb4
hocgin <hocgin@gmail.com>
2020-08-18 23:59:31 +0800

    SQL

--- spring-boot-samples/sso-server-spring-boot-sample/src/main/resources/provider_mysql.sql
+++ spring-boot-samples/sso-server-spring-boot-sample/src/main/resources/provider_mysql.sql
@@ -76,3 +76,10 @@ create table oauth_approvals
     lastModifiedAt TIMESTAMP
         COMMENT '最终修改时间'
 );
+
+INSERT INTO oauth_client_details(client_id,
+                                 resource_ids, client_secret, scope, authorized_grant_types,
+                                 web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity,
+                                 additional_information, autoapprove)
+VALUES ('client_example', null, 'hocgin', 'user_info', 'client_credentials,password,authorization_code,refresh_token',
+        'http://example.hocgin.local:20001/login/oauth2/code/', 'ROLE_USER', 1800, 86400, null, true);
