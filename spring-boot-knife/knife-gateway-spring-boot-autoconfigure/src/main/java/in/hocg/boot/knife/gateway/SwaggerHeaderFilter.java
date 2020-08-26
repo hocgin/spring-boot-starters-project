c890e56419eca0f16e1f0b9b712d89df62c8c4dc
hocgin <hocgin@gmail.com>
2020-08-26 23:08:28 +0800

    优化处理

--- spring-boot-knife/knife-gateway-spring-boot-autoconfigure/src/main/java/in/hocg/boot/knife/gateway/SwaggerHeaderFilter.java
+++ spring-boot-knife/knife-gateway-spring-boot-autoconfigure/src/main/java/in/hocg/boot/knife/gateway/SwaggerHeaderFilter.java
@@ -24,8 +24,7 @@ public class SwaggerHeaderFilter extends AbstractGatewayFilterFactory {
             if (!StringUtils.endsWithIgnoreCase(path, URI)) {
                 return chain.filter(exchange);
             }
-            String basePath = path.substring(0, path.lastIndexOf(URI));
-            ServerHttpRequest newRequest = request.mutate().header(HEADER_NAME, basePath).build();
+            ServerHttpRequest newRequest = request.mutate().build();
             ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
             return chain.filter(newExchange);
         };
