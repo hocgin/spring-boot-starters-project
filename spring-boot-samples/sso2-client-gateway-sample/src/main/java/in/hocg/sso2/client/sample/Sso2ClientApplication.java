cd28aec0e5b3ac8e9827b2a04c1fc181316bc90c
hocgin <hocgin@gmail.com>
2020-09-02 10:38:13 +0800

    单点登录初步完成

---
+++ spring-boot-samples/sso2-client-gateway-sample/src/main/java/in/hocg/sso2/client/sample/Sso2ClientApplication.java
@@ -0,0 +1,28 @@
+package in.hocg.sso2.client.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+/**
+ * Created by hocgin on 2020/8/27
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@RestController
+@SpringBootApplication
+public class Sso2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sso2ClientApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal s(Principal principal) {
+        return principal;
+    }
+}
---
+++ spring-boot-samples/sso2-server-spring-boot-sample/src/main/java/in/hocg/sso2/server/sample/Sso2ServerApplication.java
@@ -0,0 +1,19 @@
+package in.hocg.sso2.server.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hocgin on 2020/8/27
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@SpringBootApplication
+public class Sso2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sso2ServerApplication.class, args);
+    }
+
+}
