f8d042587c2c2951e7671b3b726daa8d0ad42d73
hocgin <hocgin@gmail.com>
2020-10-15 00:23:47 +0800

    UPDATE#Validation

---
+++ spring-boot-validation/validation-spring-boot-autoconfigure/src/main/java/in/hocg/boot/validation/autoconfigure/ValidationProperties.java
@@ -0,0 +1,24 @@
+package in.hocg.boot.validation.autoconfigure;
+
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by hocgin on 2020/10/15
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@Data
+@Accessors(chain = true)
+@ToString
+@ConfigurationProperties(prefix = ValidationProperties.PREFIX)
+public class ValidationProperties {
+    public static final String PREFIX = "boot.sms";
+    /**
+     * 开启状态
+     */
+    private Boolean enabled;
+}
