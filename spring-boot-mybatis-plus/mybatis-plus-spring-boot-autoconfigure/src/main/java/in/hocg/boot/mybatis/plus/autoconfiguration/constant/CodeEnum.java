c46ea3887b42ade0a7f77c06fe9c03fd832a14f6
hocgin <hocgin@gmail.com>
2020-10-14 18:14:17 +0800

    UPDATE#数据字典

---
+++ spring-boot-mybatis-plus/mybatis-plus-spring-boot-autoconfigure/src/main/java/in/hocg/boot/mybatis/plus/autoconfiguration/constant/CodeEnum.java
@@ -0,0 +1,50 @@
+package in.hocg.boot.mybatis.plus.autoconfiguration.constant;
+
+import in.hocg.boot.mybatis.plus.autoconfiguration.utils.LangUtils;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+/**
+ * Created by hocgin on 2020/2/16.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+public interface CodeEnum {
+
+    Serializable getCode();
+
+    default boolean eq(Serializable val) {
+        final Serializable code = this.getCode();
+        if (code instanceof String) {
+            return LangUtils.equals((String) code, (String) val);
+        } else if (code instanceof Integer) {
+            return LangUtils.equals((Integer) code, (Integer) val);
+        }
+        return code == val;
+    }
+
+    static <T extends CodeEnum> T ofThrow(Serializable code, Class<T> enumClass) {
+        final Optional<T> enumOpl = of(code, enumClass);
+        if (enumOpl.isPresent()) {
+            return enumOpl.get();
+        } else {
+            throw new IllegalArgumentException("未找到匹配的类型");
+        }
+    }
+
+    static <T extends CodeEnum> Optional<T> of(Serializable code, Class<T> enumClass) {
+        final T[] constants = enumClass.getEnumConstants();
+        for (T value : constants) {
+            if (value.eq(code)) {
+                return Optional.of(value);
+            }
+        }
+        return Optional.empty();
+    }
+
+    static <T extends Enum<T>> Optional<T> of(String name, Class<T> enumClass) {
+        return Optional.of(Enum.valueOf(enumClass, name));
+    }
+}
---
+++ spring-boot-mybatis-plus/mybatis-plus-spring-boot-autoconfigure/src/main/java/in/hocg/boot/mybatis/plus/autoconfiguration/constant/DataDictEnum.java
@@ -0,0 +1,11 @@
+package in.hocg.boot.mybatis.plus.autoconfiguration.constant;
+
+/**
+ * Created by hocgin on 2020/6/13.
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+public interface DataDictEnum extends CodeEnum {
+    String getName();
+}
