9909d62a585ed2c9a2483ac7cf9c8624ed4462af
hocgin <hocgin@gmail.com>
2020-11-21 23:55:18 +0800

    # Update

--- spring-boot-web/web-core/src/main/java/in/hocg/boot/web/result/ExceptionResult.java
+++ spring-boot-web/web-core/src/main/java/in/hocg/boot/web/result/ExceptionResult.java
@@ -1,8 +1,10 @@
 package in.hocg.boot.web.result;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
  * Created by hocgin on 2020/8/15
@@ -11,17 +13,21 @@ import lombok.RequiredArgsConstructor;
  * @author hocgin
  */
 @Data
-@RequiredArgsConstructor
-public class ExceptionResult {
+@Accessors(chain = true)
+@ApiModel("异常响应")
+@EqualsAndHashCode(callSuper = true)
+public class ExceptionResult<T> extends Result<T> {
     @ApiModelProperty(value = "状态码", example = "500")
-    private final Integer status;
-    @ApiModelProperty(value = "错误信息", example = "服务异常")
-    private final String message;
+    private Integer status;
     @ApiModelProperty(value = "时间戳", example = "10000000")
-    private final Long timestamp;
+    private Long timestamp;
 
-    public static ExceptionResult create(int status, String message) {
-        return new ExceptionResult(status, message, System.currentTimeMillis());
+    public static <T> ExceptionResult<T> create(int status, String message) {
+        ExceptionResult<T> result = (ExceptionResult<T>) ExceptionResult.fail();
+        result.setStatus(status);
+        result.setMessage(message);
+        result.setTimestamp(System.currentTimeMillis());
+        return result;
     }
 
 }
