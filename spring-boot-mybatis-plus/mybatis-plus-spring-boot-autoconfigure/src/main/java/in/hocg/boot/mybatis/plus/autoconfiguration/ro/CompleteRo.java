e85fcccaf316c437678441c01c59bb09ba9b10e0
hocgin <hocgin@gmail.com>
2020-12-16 22:45:42 +0800

    # 优化

--- spring-boot-mybatis-plus/mybatis-plus-spring-boot-autoconfigure/src/main/java/in/hocg/boot/mybatis/plus/autoconfiguration/ro/CompleteRo.java
+++ spring-boot-mybatis-plus/mybatis-plus-spring-boot-autoconfigure/src/main/java/in/hocg/boot/mybatis/plus/autoconfiguration/ro/CompleteRo.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
  * Created by hocgin on 2020/3/31.
@@ -12,6 +13,7 @@ import lombok.EqualsAndHashCode;
  * @author hocgin
  */
 @Data
+@Accessors(chain = true)
 @EqualsAndHashCode(callSuper = true)
 public class CompleteRo extends BasicRo {
     private int size = 30;
