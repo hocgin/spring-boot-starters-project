63991d84cbf7c4cdead9de203739d15c67354819
hocgin <hocgin@gmail.com>
2020-12-17 20:27:34 +0800

    # 数据字典

--- spring-boot-mybatis-plus/mybatis-plus-spring-boot-autoconfigure/src/main/java/in/hocg/boot/mybatis/plus/autoconfiguration/constant/CodeEnum.java
+++ spring-boot-mybatis-plus/mybatis-plus-spring-boot-autoconfigure/src/main/java/in/hocg/boot/mybatis/plus/autoconfiguration/constant/CodeEnum.java
@@ -13,26 +13,4 @@ import java.util.Optional;
  */
 public interface CodeEnum extends ICode {
 
-    static <T extends CodeEnum> T ofThrow(Serializable code, Class<T> enumClass) {
-        final Optional<T> enumOpl = of(code, enumClass);
-        if (enumOpl.isPresent()) {
-            return enumOpl.get();
-        } else {
-            throw new IllegalArgumentException("未找到匹配的类型");
-        }
-    }
-
-    static <T extends CodeEnum> Optional<T> of(Serializable code, Class<T> enumClass) {
-        final T[] constants = enumClass.getEnumConstants();
-        for (T value : constants) {
-            if (value.eq(code)) {
-                return Optional.of(value);
-            }
-        }
-        return Optional.empty();
-    }
-
-    static <T extends Enum<T>> Optional<T> of(String name, Class<T> enumClass) {
-        return Optional.of(Enum.valueOf(enumClass, name));
-    }
 }
--- spring-boot-validation/validation-spring-boot-autoconfigure/src/main/java/in/hocg/boot/validation/autoconfigure/core/ICode.java
+++ spring-boot-validation/validation-spring-boot-autoconfigure/src/main/java/in/hocg/boot/validation/autoconfigure/core/ICode.java
@@ -3,6 +3,7 @@ package in.hocg.boot.validation.autoconfigure.core;
 import in.hocg.boot.utils.LangUtils;
 
 import java.io.Serializable;
+import java.util.Optional;
 
 /**
  * Created by hocgin on 2020/10/15
@@ -22,5 +23,28 @@ public interface ICode {
             return LangUtils.equals((Integer) code, (Integer) val);
         }
         return false;
+    }
+
+    static <T extends ICode> T ofThrow(Serializable code, Class<T> enumClass) {
+        final Optional<T> enumOpl = of(code, enumClass);
+        if (enumOpl.isPresent()) {
+            return enumOpl.get();
+        } else {
+            throw new IllegalArgumentException("未找到匹配的类型");
+        }
+    }
+
+    static <T extends ICode> Optional<T> of(Serializable code, Class<T> enumClass) {
+        final T[] constants = enumClass.getEnumConstants();
+        for (T value : constants) {
+            if (value.eq(code)) {
+                return Optional.of(value);
+            }
+        }
+        return Optional.empty();
+    }
+
+    static <T extends Enum<T>> Optional<T> of(String name, Class<T> enumClass) {
+        return Optional.of(Enum.valueOf(enumClass, name));
     }
 }
