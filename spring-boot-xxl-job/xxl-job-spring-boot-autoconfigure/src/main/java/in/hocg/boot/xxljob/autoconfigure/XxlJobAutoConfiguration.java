208d95f90c9dd13347df9b7603bb4d75a31f4401
hocgin <hocgin@gmail.com>
2021-01-23 10:14:17 +0800

    # update

---
+++ spring-boot-xxl-job/xxl-job-spring-boot-autoconfigure/src/main/java/in/hocg/boot/xxljob/autoconfigure/XxlJobAutoConfiguration.java
@@ -0,0 +1,48 @@
+package in.hocg.boot.xxljob.autoconfigure;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+/**
+ * Created by hocgin on 2020/8/4
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@Slf4j
+@Configuration
+@EnableConfigurationProperties(XxlJobProperties.class)
+@RequiredArgsConstructor(onConstructor = @__(@Lazy))
+public class XxlJobAutoConfiguration {
+    private final XxlJobProperties properties;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        log.info(">>>>>>>>>>> xxl-job config init.");
+        String adminAddresses = properties.getAdminAddresses();
+        String address = properties.getAddress();
+        String appname = properties.getAppname();
+        String ip = properties.getIp();
+        Integer port = properties.getPort();
+        String accessToken = properties.getAccessToken();
+        String logPath = properties.getLogPath();
+        Integer logRetentionDays = properties.getLogRetentionDays();
+
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setAddress(address);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+
+        return xxlJobSpringExecutor;
+    }
+}
