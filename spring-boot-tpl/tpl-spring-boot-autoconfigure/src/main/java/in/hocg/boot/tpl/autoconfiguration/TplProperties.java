74052748c87c9d71e5d271470f4ba6aeae7f20ed
hocgin <hocgin@gmail.com>
2021-03-21 09:50:13 +0800

    TPL

---
+++ spring-boot-tpl/tpl-spring-boot-autoconfigure/src/main/java/in/hocg/boot/tpl/autoconfiguration/TplProperties.java
@@ -0,0 +1,17 @@
+package in.hocg.boot.tpl.autoconfiguration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by hocgin on 2020/8/15
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@Data
+@ConfigurationProperties(TplProperties.PREFIX)
+public class TplProperties {
+    public static final String PREFIX = "boot.tpl";
+
+}
