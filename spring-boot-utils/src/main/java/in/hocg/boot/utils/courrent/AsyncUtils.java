67b785d67f5133ba70c15950ed41691c6960d129
hocgin <hocgin@gmail.com>
2021-06-24 21:24:32 +0800

    feat: 1.0.7

---
+++ spring-boot-utils/src/main/java/in/hocg/boot/utils/courrent/AsyncUtils.java
@@ -0,0 +1,14 @@
+package in.hocg.boot.utils.courrent;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Created by hocgin on 2021/6/24
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+@UtilityClass
+public class AsyncUtils {
+
+}
