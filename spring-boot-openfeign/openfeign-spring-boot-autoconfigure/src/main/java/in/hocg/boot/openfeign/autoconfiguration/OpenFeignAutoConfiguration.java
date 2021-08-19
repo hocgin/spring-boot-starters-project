a317fb6a4a8cc5331bbc4528ea4aaa8fca6dce00
hocgin <hocgin@gmail.com>
2021-08-19 22:38:58 +0800

    feat: 优化

--- spring-boot-openfeign/openfeign-spring-boot-autoconfigure/src/main/java/in/hocg/boot/openfeign/autoconfiguration/OpenFeignAutoConfiguration.java
+++ spring-boot-openfeign/openfeign-spring-boot-autoconfigure/src/main/java/in/hocg/boot/openfeign/autoconfiguration/OpenFeignAutoConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.ribbon.FeignRibbonClientAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +26,6 @@ import java.util.stream.Collectors;
  * @author hocgin
  */
 @Configuration
-@EnableCaching
 @EnableConfigurationProperties({OpenFeignProperties.class})
 @ConditionalOnClass(FeignRibbonClientAutoConfiguration.class)
 @RequiredArgsConstructor(onConstructor = @__(@Lazy))
