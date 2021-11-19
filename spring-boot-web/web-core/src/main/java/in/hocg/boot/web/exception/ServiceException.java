666d6cc02b4a8689443564c8ab03fe34fb7dfb1d
hocgin <hocgin@gmail.com>
2021-11-19 23:02:00 +0800

    feat: ServiceException.java 扩展

--- spring-boot-web/web-core/src/main/java/in/hocg/boot/web/exception/ServiceException.java
+++ spring-boot-web/web-core/src/main/java/in/hocg/boot/web/exception/ServiceException.java
@@ -4,6 +4,8 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpStatus;
 import lombok.Getter;
 
+import java.util.function.Supplier;
+
 /**
  * Created by hocgin on 2019-09-24.
  * email: hocgin@gmail.com
@@ -19,15 +21,19 @@ public class ServiceException extends RuntimeException {
         this.code = code;
     }
 
+    public static Supplier<ServiceException> supplier(CharSequence template, Object... args) {
+        return () -> ServiceException.wrap(StrUtil.format(template, args));
+    }
+
     public static ServiceException wrap(Exception e) {
         return wrap(e.getMessage());
     }
 
-    public static ServiceException wrap(String message, Object... args) {
+    public static ServiceException wrap(CharSequence message, Object... args) {
         return wrap(HttpStatus.HTTP_INTERNAL_ERROR, message, args);
     }
 
-    public static ServiceException wrap(int code, String message, Object... args) {
+    public static ServiceException wrap(int code, CharSequence message, Object... args) {
         return new ServiceException(code, StrUtil.format(message, args));
     }
 
