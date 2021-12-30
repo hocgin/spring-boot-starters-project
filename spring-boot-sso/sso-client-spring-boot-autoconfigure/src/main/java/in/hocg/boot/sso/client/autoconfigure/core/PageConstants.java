863bb90aab4a0daadaacb0ecf296976f5b6041b9
hocgin <hocgin@gmail.com>
2021-12-30 16:51:19 +0800

    feat: sso client logout

---
+++ spring-boot-sso/sso-client-spring-boot-autoconfigure/src/main/java/in/hocg/boot/sso/client/autoconfigure/core/PageConstants.java
@@ -0,0 +1,13 @@
+package in.hocg.boot.sso.client.autoconfigure.core;
+
+/**
+ * Created by hocgin on 2021/12/30
+ * email: hocgin@gmail.com
+ *
+ * @author hocgin
+ */
+public class PageConstants {
+    public static final String LOGIN_URL = "https://sso.hocgin.top/login";
+    public static final String LOGOUT_URL = "/logout";
+
+}
